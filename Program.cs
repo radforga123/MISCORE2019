682384ae0775d407cbe6ea4a858893f441fbff49
Radforga <radmir2301@gmail.com>
2024-06-09 19:51:09 +0300

    Nfr

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace MISCORE2019
 {
-    public class Program //Запустилась сучка
+    public class Program 
     {
 
         public static void Main(string[] args)
