87cc0dadcc76a7163fbefe9c1274cd038c2ca6e8
zoloklp <tairsamigullin@gmail.com>
2024-06-11 20:27:07 +0300

    врачи x2

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MISCORE2019.Models
 {
     public class Doctor
     {
         public int ID { get; set; }
+        [Display(Name = "ФИО")]
         public String FIO { get; set; }
+
+        [Display(Name = "Специализация")]
         public String specialization { get; set; }
     }
 }
