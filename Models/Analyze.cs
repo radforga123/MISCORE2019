e8f9245b74d87344c8419efcaa9c37dd63326ea3
zoloklp <tairsamigullin@gmail.com>
2024-06-12 18:36:31 +0300

    анализы

--- Models/Analyze.cs
+++ Models/Analyze.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace MISCORE2019.Models
     public class Analyze
     {
         public int ID { get; set; }
+
+        [Display(Name = "Группа крови")]
         public int bloodGroup { get; set; }
+
+        [Display(Name = "Эпилептическая активность")]
         public String epileptiphoricActivity { get; set; }
         public int PatientID { get; set; }
         public Patient Patient { get; set; }
