70676793dfb4242d0a50c349b3d5d9fbd5cca38e
zoloklp <tairsamigullin@gmail.com>
2024-06-14 16:33:14 +0300

    посещения и анализы

--- Models/Visit.cs
+++ Models/Visit.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,8 @@ namespace MISCORE2019.Models
     public class Visit
     {
         public int ID { get; set; }
+
+        [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime time { get; set; }
         public List<Doctor> doctors { get; set; }
         public int PatientID { get; set; }
